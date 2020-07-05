863f702362904413345605fc42298819a54f2226
Masao Kitamura <masaok@gmail.com>
2020-07-05 01:41:37 -0700

    Tutorial copy/pasta renders a page now, but no styling

---
+++ src/Components/Application.js
@@ -0,0 +1,20 @@
+import React from 'react'
+// import { Router } from 'react-router-dom'
+import { BrowserRouter as Router } from 'react-router-dom'
+import SignIn from './SignIn'
+import SignUp from './SignUp'
+import ProfilePage from './ProfilePage'
+import PasswordReset from './PasswordReset'
+function Application() {
+  const user = null
+  return user ? (
+    <ProfilePage />
+  ) : (
+    <Router>
+      <SignUp path="signUp" />
+      <SignIn path="/" />
+      <PasswordReset path="passwordReset" />
+    </Router>
+  )
+}
+export default Application
