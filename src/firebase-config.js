92661908762a45486672c6c3ac178b27ca9a0066
Masao Kitamura <masaok@gmail.com>
2020-07-14 08:04:32 -0700

    Allowing firebase config to be pushed

---
+++ src/firebase-config.js
@@ -0,0 +1,11 @@
+// Firebase Console > Project Settings > General > Your apps
+// OK to push to repo because it would be public in HTML source anyway
+export const firebaseConfig = {
+  apiKey: 'AIzaSyAffJepbk0yV6gSTIV2e6_T_1DsDwwcMp0',
+  authDomain: 'react-firebase-auth-demo-80cd2.firebaseapp.com',
+  databaseURL: 'https://react-firebase-auth-demo-80cd2.firebaseio.com',
+  projectId: 'react-firebase-auth-demo-80cd2',
+  storageBucket: 'react-firebase-auth-demo-80cd2.appspot.com',
+  messagingSenderId: '546798372784',
+  appId: '1:546798372784:web:315b653e9e8aab5beaf875'
+}
